0bb75fc9c4529f3cd7f8c430a51f98ea497c09f5
Petrusfp <111288826+Petrusfp@users.noreply.github.com>
2022-08-17 12:31:40 +0300

    Delete ex007.py

--- ex007.py
+++
@@ -1,4 +0,0 @@
-n1 = float(input('Digite sua primeira nota em matemática nesse semestre logo ao lado >>>>> '))
-n2 = float(input('Digite sua segunda nota em matemática nesse semestre >>>>> '))
-s = n1 + n2
-print(f'A sua média em matemática esse semestre é: {s/2}')
